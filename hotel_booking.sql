a32df822cd6518046fd2e09fe52113c4069504b1
Steffi <steffiwidjaya01@gmail.com>
2025-07-04 14:45:22 +0700

    add log history

--- hotel_booking.sql
+++ hotel_booking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2025 at 11:18 AM
+-- Generation Time: Jul 04, 2025 at 09:44 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -49,6 +49,23 @@ CREATE TABLE `rentals` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `rental_logs`
+--
+
+CREATE TABLE `rental_logs` (
+  `id` int(11) NOT NULL,
+  `rental_id` int(11) NOT NULL,
+  `action` varchar(20) DEFAULT NULL,
+  `field_changed` varchar(50) DEFAULT NULL,
+  `old_value` text DEFAULT NULL,
+  `new_value` text DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `time_stamp` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -63,7 +80,6 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`email`, `password`, `nib`) VALUES
-('def@gmail.com', '$2b$10$2xj8uQaF7ZZZD1hyM2ED4uofrM8gsIqGMgpeSFdurEASUvZhmyn/2', '1234567891234'),
 ('steffiwidjaya01@gmail.com', '$2a$10$o8k3V7RT0Yd2DNj5.YZVyelEBYENz/Z9OGcCPXqgOnldGmzNxr5hy', NULL);
 
 --
@@ -77,6 +93,14 @@ ALTER TABLE `rentals`
   ADD PRIMARY KEY (`id`),
   ADD KEY `email_agent` (`email_agent`),
   ADD KEY `diedit_oleh` (`diedit_oleh`);
+
+--
+-- Indexes for table `rental_logs`
+--
+ALTER TABLE `rental_logs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `rental_id` (`rental_id`),
+  ADD KEY `email` (`email`);
 
 --
 -- Indexes for table `users`
@@ -95,6 +119,12 @@ ALTER TABLE `rentals`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `rental_logs`
+--
+ALTER TABLE `rental_logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -104,6 +134,13 @@ ALTER TABLE `rentals`
 ALTER TABLE `rentals`
   ADD CONSTRAINT `rentals_ibfk_1` FOREIGN KEY (`email_agent`) REFERENCES `users` (`email`),
   ADD CONSTRAINT `rentals_ibfk_2` FOREIGN KEY (`diedit_oleh`) REFERENCES `users` (`email`);
+
+--
+-- Constraints for table `rental_logs`
+--
+ALTER TABLE `rental_logs`
+  ADD CONSTRAINT `rental_logs_ibfk_1` FOREIGN KEY (`rental_id`) REFERENCES `rentals` (`id`),
+  ADD CONSTRAINT `rental_logs_ibfk_2` FOREIGN KEY (`email`) REFERENCES `users` (`email`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
