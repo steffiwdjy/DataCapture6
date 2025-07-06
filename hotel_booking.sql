364e3c754fbaa43bc4d441525e75433380706c25
Steffi <steffiwidjaya01@gmail.com>
2025-07-06 17:27:43 +0700

    update database

--- hotel_booking.sql
+++ hotel_booking.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 04, 2025 at 09:44 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Waktu pembuatan: 06 Jul 2025 pada 12.26
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rentals`
+-- Struktur dari tabel `rentals`
 --
 
 CREATE TABLE `rentals` (
@@ -43,13 +43,14 @@ CREATE TABLE `rentals` (
   `lama_menginap` int(11) DEFAULT NULL,
   `komentar` text DEFAULT NULL,
   `email_agent` varchar(255) DEFAULT NULL,
-  `diedit_oleh` varchar(255) DEFAULT NULL
+  `diedit_oleh` varchar(255) DEFAULT NULL,
+  `jenis_sewa` enum('Harian','Mingguan','Bulanan') DEFAULT 'Harian'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rental_logs`
+-- Struktur dari tabel `rental_logs`
 --
 
 CREATE TABLE `rental_logs` (
@@ -66,7 +67,7 @@ CREATE TABLE `rental_logs` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -76,7 +77,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`email`, `password`, `nib`) VALUES
@@ -87,7 +88,7 @@ INSERT INTO `users` (`email`, `password`, `nib`) VALUES
 --
 
 --
--- Indexes for table `rentals`
+-- Indeks untuk tabel `rentals`
 --
 ALTER TABLE `rentals`
   ADD PRIMARY KEY (`id`),
@@ -95,7 +96,7 @@ ALTER TABLE `rentals`
   ADD KEY `diedit_oleh` (`diedit_oleh`);
 
 --
--- Indexes for table `rental_logs`
+-- Indeks untuk tabel `rental_logs`
 --
 ALTER TABLE `rental_logs`
   ADD PRIMARY KEY (`id`),
@@ -103,40 +104,40 @@ ALTER TABLE `rental_logs`
   ADD KEY `email` (`email`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`email`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `rentals`
+-- AUTO_INCREMENT untuk tabel `rentals`
 --
 ALTER TABLE `rentals`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `rental_logs`
+-- AUTO_INCREMENT untuk tabel `rental_logs`
 --
 ALTER TABLE `rental_logs`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `rentals`
+-- Ketidakleluasaan untuk tabel `rentals`
 --
 ALTER TABLE `rentals`
   ADD CONSTRAINT `rentals_ibfk_1` FOREIGN KEY (`email_agent`) REFERENCES `users` (`email`),
   ADD CONSTRAINT `rentals_ibfk_2` FOREIGN KEY (`diedit_oleh`) REFERENCES `users` (`email`);
 
 --
--- Constraints for table `rental_logs`
+-- Ketidakleluasaan untuk tabel `rental_logs`
 --
 ALTER TABLE `rental_logs`
   ADD CONSTRAINT `rental_logs_ibfk_1` FOREIGN KEY (`rental_id`) REFERENCES `rentals` (`id`),
